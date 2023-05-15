09bba1db922a9f7b929335ebd31673baa0486daf
emilia-szymanska <emilia.szymanska.eka@gmail.com>
2023-05-15 22:31:43 +0200

    Add loading functions in a separate file

---
+++ mv3dpose/config.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.insert(0, '../')
+from os.path import isdir, join
+from os import makedirs
+import json
+import shutil
+
+class Config:
+    def __init__(self, dataset_dir):
+
+        dataset_json = join(dataset_dir, 'dataset.json')
+        settings = json.load(open(dataset_json))
+
+        self.output_dir = join(dataset_dir, settings['output_dir'])  
+        self.json_poses_prefix = settings['json_poses_prefix']
+        self.cam_dir     = join(dataset_dir, settings['camera_calib_dir'])
+        self.kyp_dir     = join(dataset_dir, settings['keypoints_dir'])
+        self.scale_to_mm = settings['scale_to_mm']
+        self.n_cameras   = settings['n_cameras']
+
+        assert isdir(self.cam_dir)
+        assert isdir(self.kyp_dir)
+
+        if isdir(self.output_dir):
+            print('\n[deleting existing output directory...]')
+            shutil.rmtree(self.output_dir)
+        makedirs(self.output_dir)
+
+        # ~~~~~~~~~~~~~~~~~~~~~~
+
+        self.smoothing_interpolation_range = settings['smoothing_interpolation_range']
+        self.max_distance_between_tracks = settings['max_distance_between_tracks']
+        self.epi_threshold    = settings['epi_threshold']
+        self.min_track_length = settings['min_track_length']
+        self.merge_distance   = settings['merge_distance']
+        self.last_seen_delay  = settings['last_seen_delay']
+        self.smoothing_sigma  = settings['smoothing_sigma']
+        self.do_smoothing     = settings['do_smoothing']
+        self.valid_frames     = list(range(settings['valid_frames']))
--- mv3dpose/mvpose.py
+++ mv3dpose/mvpose.py
@@ -2,6 +2,8 @@ import sys
 sys.path.insert(0, '../')
 from mv3dpose.data.openpose import OpenPoseKeypoints, MultiOpenPoseKeypoints
 from mv3dpose.tracking import tracking, Track
+from mv3dpose.config import Config
+from mv3dpose.load_functions import load_keypoints, load_cameras
 import mv3dpose.geometry.camera as camera
 from os.path import isdir, join, isfile
 from os import makedirs, listdir
@@ -9,195 +11,62 @@ from tqdm import tqdm
 from time import time
 import json
 import numpy as np
-import shutil
-
-dataset_dir = '/home/emilia/digital_humans/project/lisst_mv3dpose/dataset'
-output_dir = join(dataset_dir, 'output')
-dataset_json = join(dataset_dir, 'dataset.json')
-json_poses_prefix = 'pizza_00_'
-
-# vid_dir = join(dataset_dir, 'openpose_keypoints')
-cam_dir = join(dataset_dir, 'cameras')
-kyp_dir = join(dataset_dir, 'openpose_keypoints')
-
-# assert isdir(vid_dir)
-assert isdir(cam_dir)
-
-if isdir(output_dir):
-    print('\n[deleting existing output directory...]')
-    shutil.rmtree(output_dir)
-
-makedirs(output_dir)
-
-# ~~~~~~~~~~~~~~~~~~~~~~
-
-Settings = json.load(open(dataset_json))
-
-scale_to_mm = Settings['scale_to_mm']
-n_cameras = Settings['n_cameras']
 
 
-def get_optional(key, default_value):
-    if key in Settings:
-        return Settings[key]
-    else:
-        return default_value
+if __name__ == '__main__':
+    
+    # the first argument is the path to dataset dir  
+    dataset_dir = sys.argv[1]
+    conf = Config(dataset_dir)
 
+    print("\n#frames", len(conf.valid_frames))
 
-epi_threshold = get_optional(
-    'epi_threshold', 80)
-max_distance_between_tracks = get_optional(
-    'max_distance_between_tracks', 80)
-min_track_length = get_optional(
-    'min_track_length', 5)
-merge_distance = get_optional(
-    'merge_distance', 80)
-last_seen_delay = get_optional(
-    'last_seen_delay', 15)
-smoothing_sigma = get_optional(
-    'smoothing_sigma', 2)
-smoothing_interpolation_range = get_optional(
-    'smoothing_interpolation_range', 50)
-do_smoothing = get_optional(
-    'do_smoothing', True
-)
+    pe    = load_keypoints(conf)
+    calib = load_cameras(conf)
 
-if 'valid_frames' in Settings:
-    valid_frames = Settings['valid_frames']
-else:
-    # take all frames in the setup
-    files = listdir(join(kyp_dir, f'{json_poses_prefix}00_json'))
-    valid_frames = list(range(len(files)))
+    # ~~~~~~~~~~~~~~~~~~~~~~~
+    # L O A D  2 D  P O S E S
+    # ~~~~~~~~~~~~~~~~~~~~~~~
+    print('\n[load 2d poses]')
+    poses_per_frame = []
+    for frame in tqdm(conf.valid_frames):
+        predictions = pe.predict(frame)
+        poses_per_frame.append(predictions)
 
-print("\n#frames", len(valid_frames))
+    # ~~~~~~~~~~~~~~~
+    # T R A C K I N G
+    # ~~~~~~~~~~~~~~~
+    print('\n[tracking]')
+    _start = time()
+    tracks = tracking(calib, poses_per_frame,
+                    epi_threshold=conf.epi_threshold,
+                    scale_to_mm=conf.scale_to_mm,
+                    max_distance_between_tracks=conf.max_distance_between_tracks,
+                    actual_frames=conf.valid_frames,
+                    min_track_length=conf.min_track_length,
+                    merge_distance=conf.merge_distance,
+                    last_seen_delay=conf.last_seen_delay)
+    _end = time()
+    print('\telapsed', _end - _start)
+    print("\n\t# detected tracks:", len(tracks))
 
-# ~~~~~~~~~~~~~~~~~
-# K E Y P O I N T S
-# ~~~~~~~~~~~~~~~~~
-print('\n[load keypoints]')
-keypoints = []
-for cid in tqdm(range(n_cameras)):
-    loc = join(kyp_dir, f'{json_poses_prefix}%02d_json' % cid)
-    assert isdir(loc), loc
-    file_prefix = f'{json_poses_prefix}%02d' % cid
-    pe = OpenPoseKeypoints(f'{file_prefix}_%012d', loc)
-    keypoints.append(pe)
+    # ~~~~~~~~~~~~~~~
+    # S M O O T H I N G
+    # ~~~~~~~~~~~~~~~
+    if conf.do_smoothing:
+        print('\n[smoothing]')
+        tracks_ = []
+        for track in tqdm(tracks):
+            track = Track.smoothing(track,
+                                    sigma=conf.smoothing_sigma,
+                                    interpolation_range=conf.smoothing_interpolation_range)
+            tracks_.append(track)
+        tracks = tracks_
 
-pe = MultiOpenPoseKeypoints(keypoints)
-
-# ~~~~~~~~~~~~~
-# C A M E R A S
-# ~~~~~~~~~~~~~
-print('\n[load cameras]')
-Calib = []  # { n_frames x n_cameras }
-try:
-    # assumption: cameras do not change over time
-    for cid in range(n_cameras):
-        cam_fname = join(cam_dir, 'camera%02d_calibration.json' % cid)
-        assert isfile(cam_fname), cam_fname
-        cam = camera.Camera.load_from_file(cam_fname)
-        Calib.append(cam)
-except AssertionError:
-    print('\tnew version of cameras is used...')
-
-    class CamerasPerFrame:
-
-        def __init__(self, cam_dir, n_cameras, valid_frames):
-            self.n_cameras = n_cameras
-            self.n_frames = len(valid_frames)
-            self.first_frame = valid_frames[0]
-            self.cameras = []
-            for cid in range(n_cameras):
-                camfile = join(cam_dir, 'camera%02d.json' % cid)
-                with open(camfile, 'r') as f:
-                    cam_as_dict_list = json.load(f)
-
-                cam_as_object_list = []
-                for cam in cam_as_dict_list:
-                    start_frame = cam['start_frame']
-                    end_frame = cam['end_frame']
-                    K = np.array(cam['K'])
-                    rvec = np.array(cam['rvec'])
-                    tvec = np.array(cam['tvec'])
-                    distCoef = np.array(cam['distCoef'])
-                    w = int(cam['w'])
-                    h = int(cam['h'])
-                    cam = camera.ProjectiveCamera(K, rvec, tvec, distCoef, w, h)
-
-                    cam_as_object_list.append({
-                        "start_frame": start_frame,
-                        "end_frame": end_frame,
-                        "cam": cam
-                    })
-
-                self.cameras.append(cam_as_object_list)
-
-        def __getitem__(self, frame):
-            """
-            :param frame: frame, starting at 0!
-            """
-            frame += self.first_frame
-
-            cameras = [1] * self.n_cameras
-            for cid, cam_as_object_list in enumerate(self.cameras):
-                for cam in cam_as_object_list:
-                    start_frame = cam['start_frame']
-                    end_frame = cam['end_frame']
-                    if start_frame <= frame <= end_frame:
-                        cameras[cid] = cam['cam']
-                        break
-            for cam in cameras:
-                assert cam != 1
-            return cameras
-
-        def __len__(self):
-            return self.n_frames
-
-    Calib = CamerasPerFrame(cam_dir, n_cameras, valid_frames)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~
-# L O A D  2 D  P O S E S
-# ~~~~~~~~~~~~~~~~~~~~~~~
-print('\n[load 2d poses]')
-poses_per_frame = []
-for frame in tqdm(valid_frames):
-    predictions = pe.predict(frame)
-    poses_per_frame.append(predictions)
-
-# ~~~~~~~~~~~~~~~
-# T R A C K I N G
-# ~~~~~~~~~~~~~~~
-print('\n[tracking]')
-_start = time()
-tracks = tracking(Calib, poses_per_frame,
-                  epi_threshold=epi_threshold,
-                  scale_to_mm=scale_to_mm,
-                  max_distance_between_tracks=max_distance_between_tracks,
-                  actual_frames=valid_frames,
-                  min_track_length=min_track_length,
-                  merge_distance=merge_distance,
-                  last_seen_delay=last_seen_delay)
-_end = time()
-print('\telapsed', _end - _start)
-print("\n\t# detected tracks:", len(tracks))
-
-# ~~~~~~~~~~~~~~~
-# S M O O T H I N G
-# ~~~~~~~~~~~~~~~
-if do_smoothing:
-    print('\n[smoothing]')
-    tracks_ = []
-    for track in tqdm(tracks):
-        track = Track.smoothing(track,
-                                sigma=smoothing_sigma,
-                                interpolation_range=smoothing_interpolation_range)
-        tracks_.append(track)
-    tracks = tracks_
-
-# ~~~~~~~~~~~~~~~~~
-# S E R I A L I Z E
-# ~~~~~~~~~~~~~~~~~
-print('\n[serialize 3d tracks]')
-for tid, track in tqdm(enumerate(tracks)):
-    fname = join(output_dir, 'track' + str(tid) + '.json')
-    track.to_file(fname)
+    # ~~~~~~~~~~~~~~~~~
+    # S E R I A L I Z E
+    # ~~~~~~~~~~~~~~~~~
+    print('\n[serialize 3d tracks]')
+    for tid, track in tqdm(enumerate(tracks)):
+        fname = join(conf.output_dir, 'track' + str(tid) + '.json')
+        track.to_file(fname)
